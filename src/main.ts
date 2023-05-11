2504b55749872a225eba82fcb457e7da77600da1
pj <1242201332@qq.com>
2023-05-11 18:03:46 +0800

    bem

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import './assets/main.css'
+import './style/index.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -6,9 +6,12 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(ElementPlus)
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,45 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home">
+    <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+      <el-menu-item index="1">Processing Center</el-menu-item>
+      <el-sub-menu index="2">
+        <template #title>Workspace</template>
+        <el-menu-item index="2-1">item one</el-menu-item>
+        <el-menu-item index="2-2">item two</el-menu-item>
+        <el-menu-item index="2-3">item three</el-menu-item>
+        <el-sub-menu index="2-4">
+          <template #title>item four</template>
+          <el-menu-item index="2-4-1">item one</el-menu-item>
+          <el-menu-item index="2-4-2">item two</el-menu-item>
+          <el-menu-item index="2-4-3">item three</el-menu-item>
+        </el-sub-menu>
+      </el-sub-menu>
+      <el-menu-item index="3" disabled>Info</el-menu-item>
+      <el-menu-item index="4">Orders</el-menu-item>
+    </el-menu>
+    <el-backtop :right="100" :bottom="100">
+      <div style="
+        height: 100%;
+        width: 100%;
+        text-align: center;
+        line-height: 40px;
+        color: #1989fa;
+      ">
+        UP123
+      </div>
+    </el-backtop>
+  </div>
 </template>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const activeIndex = ref('1')
+const handleSelect = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
+</script>
+<style lang="scss" scoped>
+.home {
+  height: 8000px;
+}
+</style>
